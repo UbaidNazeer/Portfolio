f966e0e987901f9a55661a2ce92caca7ec8d82a3
UbaidNazeer <64394376+UbaidNazeer@users.noreply.github.com>
2020-05-05 22:58:09 +0500

    initial

--- index.php
+++ index.php
@@ -10,31 +10,43 @@
     </head>
     <body>
        <section>
-        <div class="container service text-center" id="services">
-            <h1>Services</h1>
-            <p>Our services are front end developer 2020.</p>
-            <div class="row rowsetting">
-                <div class="col-lg-4 col-sm-4 col-md-4 d-block m-auto cl1">
-                    <div class="imgsetting d-block m-auto"><i class="fa fa-shopping-cart fa-3x text-white"></i>
-</div>
-               <h2>E-Commerce</h2>
-               <p>We are developing e-commerce store.We are developing e-commerce store.We are developing e-commerce store.</p>
-                </div>
-                 <div class="col-lg-4 col-sm-4 col-md-4 d-block m-auto cl1">
-                    <div class="imgsetting d-block m-auto"><i class="fa fa-desktop fa-3x text-white"></i>
-</div>
-               <h2>Responsive Design</h2>
-               <p>We are developing e-commerce store.We are developing e-commerce store.We are developing e-commerce store.</p>
-                </div>
-                 <div class="col-lg-4 col-sm-4 col-md-4 d-block m-auto cl1">
-                    <div class="imgsetting d-block m-auto"><i class="fa fa-lock fa-3x text-white"></i>
-</div>
-               <h2>Security</h2>
-               <p>We are developing e-commerce store.We are developing e-commerce store.We are developing e-commerce store.</p>
-                </div>
-            </div>
-        </div>
-        </section>
+           <div class="container ourservice text-center" id="services">
+               <h1>Services</h1>
+               <p>We have a team of professional web developers.</p>
+               <div class="row rowsetting">
+                   <div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+                       <div class="imgsetting d-block m-auto">
+                           <i class="fa fa-shopping-cart fa-3x text-white icon"></i>
+
+                       </div>
+                       <h2>E-Commerce</h2>
+                       <p>We have a team of professional e-commerce store developers.
+                       We have a team of professional e-commerce store developers.
+                       We have a team of professional e-commerce store developers.</p>
+                   </div>
+                   <div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+                       <div class="imgsetting d-block m-auto">
+                           <i class="fa fa-desktop fa-3x text-white icon"></i>
+
+                       </div>
+                       <h2>Responsive</h2>
+                       <p>We have a team of professional e-commerce store developers.
+                       We have a team of professional e-commerce store developers.
+                       We have a team of professional e-commerce store developers.</p>
+                   </div>
+                   <div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+                       <div class="imgsetting d-block m-auto">
+                           <i class="fa fa-lock fa-3x text-white icon"></i>
+
+                       </div>
+                       <h2>Security</h2>
+                       <p>We have a team of professional e-commerce store developers.
+                       We have a team of professional e-commerce store developers.
+                       We have a team of professional e-commerce store developers.</p>
+                   </div>
+               </div>
+           </div>
+       </section>
     </body>
     
 </html>
