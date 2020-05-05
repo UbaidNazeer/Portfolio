e983969c9f955b2781528aed11e2595138a68174
UbaidNazeer <64394376+UbaidNazeer@users.noreply.github.com>
2020-05-05 12:49:11 +0500

    work

--- header.php
+++ header.php
@@ -2,20 +2,44 @@
 <html>
     <head>
         <title></title>
-        <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/header.css">
+       <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <h1>Header</h1>
+       <div class="header">
+       <div class="bgimg">
+           <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+               <div class="container">
+                   <a href="#" class="navbar-brand text-warning font-weight-bold">Portfolio</a>
+                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navtop">
+                       <span class="navbar-toggler-icon"></span>
+                   </button>
+                   <div class="collapse navbar-collapse" id="navtop">
+                       <ul class="navbar nav ml-auto">
+                           <li class="nav-item">
+                               <a href="portfolio.php" class="nav-link">Portfolio</a>
+                           </li>
+                           <li class="nav-item">
+                               <a href="services.php" class="nav-link">Services</a>
+                           </li>
+                           <li class="nav-item">
+                               <a href="about.php" class="nav-link">About</a>
+                           </li>
+                           <li class="nav-item">
+                               <a href="team.php" class="nav-link">Team</a>
+                           </li>
+                       </ul>
+                   </div>
+               </div>
+           </nav>
+       </div>
+        
+        </div>
     </body>
     
 </html>
